f68fe3f21e2896574223959b1e035e8755fa141b
vCra <aaw13@aber.ac.uk>
2018-12-04 11:36:57 +0000

    WIP

---
+++ app/src/androidTest/java/io/vcra/apps/languageflip/TestPhraseBookDB.kt
@@ -0,0 +1,47 @@
+package io.vcra.apps.languageflip
+
+import android.app.Instrumentation
+import android.arch.persistence.room.Room
+import android.content.Context
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import java.io.IOException
+
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import io.vcra.apps.languageflip.data.LFDB
+import io.vcra.apps.languageflip.data.phrasebook.PhraseBook
+import io.vcra.apps.languageflip.data.phrasebook.PhraseBookDAO
+
+import junit.framework.TestCase.assertEquals
+
+@RunWith(AndroidJUnit4::class)
+class TestPhraseBookDB {
+    private var dao: PhraseBookDAO? = null
+    private var db: LFDB? = null
+
+    @Before
+    fun createDb() {
+        val appContext = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(appContext, LFDB::class.java).build()
+        dao = db!!.phraseBookDAO()
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        db!!.close()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun createPhraseBook() {
+        val pb = PhraseBook("Phrasebook 1")
+        dao!!.insert(pb)
+        assertEquals(dao!!.count(), 1)
+    }
+}
